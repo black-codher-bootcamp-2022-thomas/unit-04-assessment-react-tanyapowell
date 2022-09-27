5441cb82e10672ab05725d95d7ce6b902ee1ed1e
Tanya Powell <hello@tanyapowell.co.uk>
2022-09-27 19:04:44 +0100

    Created About page using Route components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AboutPage from "./pages/About";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://imusicstore.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         View Example Bookcase App
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={} />
+          <Route path="about" element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
-
-export default App;
