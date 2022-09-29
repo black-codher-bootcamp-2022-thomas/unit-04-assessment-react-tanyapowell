eb751023b7076a4b88d87c02a964d9df003915f1
Tanya Powell <hello@tanyapowell.co.uk>
2022-09-29 18:33:54 +0100

    added value given to props from example data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,15 @@
+import data from "../models/example-data.json";
 import Product from "../Components/Product";
 
 function HomePage() {
-  const names = [
-    { title: "Postman Pat", author: "John Arthur Cunliffe", price: "25p" },
-    { title: "Girl Woman Other", author: "Bernardine Evaristo", price: "55p" },
-    { title: "Fireman Sam", author: "David Jones", price: "27p" },
-    { title: "The Colour Purple", author: "Alice Walker", price: "88p" },
-    {
-      title: "Murder on the Orient Express",
-      author: "Agathie Christie",
-      price: "99p",
-    },
-    { title: "Annabels Book", author: "ANNABEL", price: "150p" },
-  ];
-
   return (
     <div>
-      {names.map((individualItem) => {
+      {data.map((individualItem) => {
         return (
           <Product
-            name={individualItem.title}
-            artist={individualItem.author}
-            price={individualItem.price}
+            name={individualItem.trackName}
+            artist={individualItem.artistName}
+            price={individualItem.trackPrice}
           />
         );
       })}
