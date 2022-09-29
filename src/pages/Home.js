f1ca6739a342fbddba732c41ee1b6f1883eb1b2b
Tanya Powell <hello@tanyapowell.co.uk>
2022-09-29 19:23:42 +0100

    added an input field using states

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import data from "../models/example-data.json";
 import Product from "../Components/Product";
+import { useState } from "react";
 
 function HomePage() {
+  const [searchTerm, setSearchTerm] = useState("Type Here");
+
   return (
     <div>
+      <input onChange={(event) => setSearchTerm(event.target.value)} value={searchTerm} />
+      <p>{`The person typed: ${searchTerm}`}</p>
       {data.map((individualItem) => {
         return (
           <Product
+            key={individualItem.trackId}
             name={individualItem.trackName}
             artist={individualItem.artistName}
             price={individualItem.trackPrice}
